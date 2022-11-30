de2538a0e527da8f4ce1a06a7093a268b3e10c13
Chisom Nnabuihe <chisomnnabuihe@gmail.com>
2022-11-30 15:07:37 +0000

    Prod changes

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a1c04c05d3ae54852ae4ba161225fcb2-2038996333.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
